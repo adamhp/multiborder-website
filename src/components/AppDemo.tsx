879dc24758875d7cae208e016dfc93ab502273e9
Adam Pearce <adamp319@gmail.com>
2024-09-14 08:12:42 -0400

    Try demo mp4 instead

--- src/components/AppDemo.tsx
+++ src/components/AppDemo.tsx
@@ -7,13 +7,17 @@ import Image from 'next/image';
 export function AppDemo() {
   return (
     <AppScreen>
-      <Image
+      <video width='393' height='952' playsInline muted preload='none' autoPlay>
+        <source src='/demo.mp4' type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
+      {/* <Image
         unoptimized={true}
         alt='demo video gif'
         src={demo}
         width={393}
         height={952}
-      />
+      /> */}
     </AppScreen>
   );
 }
