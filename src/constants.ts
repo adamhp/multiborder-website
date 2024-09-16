970dc959ac52e063cd64de999e56c5ac0eb7defd
Adam Pearce <adamp319@gmail.com>
2024-09-15 20:05:56 -0400

    Upload Download link

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,10 @@
-export const appUrl = "https://apps.apple.com/us/app/multiborder/id6473557515";
+export const appUrl =
+  'https://apps.apple.com/app/apple-store/id6473557515?pt=126773719&ct=website&mt=8';
 export const nav = [
-  ["Download", appUrl],
-  ["Features", "/features"],
+  ['Download', appUrl],
+  ['Features', '/features'],
   // ['Reviews', '/reviews'],
-  ["Support", "/support"],
-  ["Feedback", "/feedback"],
-  ["About", "/about"],
+  ['Support', '/support'],
+  ['Feedback', '/feedback'],
+  ['About', '/about'],
 ];
